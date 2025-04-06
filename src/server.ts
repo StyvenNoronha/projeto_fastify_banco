4793967a9c59f2e4bd188068e2e41f444533f079
styvenNoronha <styvenn16@gmail.com>
2025-04-06 11:08:36 -0300

    Tratando env com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 import { randomUUID } from 'node:crypto'
 const app = fastify()
 
@@ -22,7 +23,7 @@ app.get('/user', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server Running')
