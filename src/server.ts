2f824153f60af629827f2e6acde46a3aeb2f2b5d
styvenNoronha <styvenn16@gmail.com>
2025-04-06 08:45:23 -0300

    Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello',()=>{
-    return 'hello world fastify'
+app.get('/hello', () => {
+  return 'hello world fastify'
 })
 
-app.listen({
-    port:3333,
-
-}).then(()=>{
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP server Running')
-})
+  })
