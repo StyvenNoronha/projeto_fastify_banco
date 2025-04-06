89e7badb82da3f5c76e35e298982a705a32c4691
styvenNoronha <styvenn16@gmail.com>
2025-04-06 14:48:12 -0300

    Utilizando cookies no Fastify

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 import { search } from './routes/search'
+import cookie from '@fastify/cookie'
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
