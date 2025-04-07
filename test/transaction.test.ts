07b09f5e60c144dea474c66c37deb7a8794dfed4
styvenNoronha <styvenn16@gmail.com>
2025-04-07 08:28:07 -0300

    Testando listagem de transações

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -2,6 +2,7 @@ import { expect, test, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 import { describe } from 'node:test'
+import { string } from 'zod'
 
 describe('transactions routes', () => {
   beforeAll(async () => {
@@ -18,5 +19,25 @@ describe('transactions routes', () => {
       .send({ title: 'test do vitest', amount: 200, type: 'credit' })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  test('listar todas as transações', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'test do vitest', amount: 200, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'test do vitest',
+        amount: 200,
+      }),
+    ])
   })
 })
